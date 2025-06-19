6821cbb348e1883b5cad48794b5c11c6d6669472
MoukilaDev <koppmouvera@yahoo.fr>
2025-06-19 21:54:40 +0100

    blog skeleton - footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 const Footer = ()=>{
     return(
         <div className="footer">
-            <p>© 2025 Pessaake, MoukilaDev. All rights reserved.</p>
-            <p>Follow us on social media!</p>
-            <ul className="social-links">
-                <li><a href="https://twitter.com/pessaake">Twitter</a></li>
-                <li><a href="https://facebook.com/pessaake">Facebook</a></li>
-                <li><a href="https://instagram.com/pessaake">Instagram</a></li>
-            </ul>
+            <div className="left-foot">
+                <p>© 2025 Pessaake, MoukilaDev. All rights reserved.</p>
+            </div>
+            
+            <div className="rigth-foot">
+            <hr/>
+                <p>Follow us on social media!</p>
+                <ul className="social-links" type="none">
+                    <li><a href="https://twitter.com/pessaake">Twitter</a></li>
+                    <li><a href="https://facebook.com/pessaake">Facebook</a></li>
+                    <li><a href="https://instagram.com/pessaake">Instagram</a></li>
+                </ul>
+            </div>
         </div>
     );
 }
